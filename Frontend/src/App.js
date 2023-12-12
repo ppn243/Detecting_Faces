e54f4deb82f7eef8275dff403eadcfa039688ec0
thaiikhang <huynhkhang690@gmail.com>
2023-12-12 13:26:32 +0700

    [UI] Initialize files and setup

---
+++ Frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
