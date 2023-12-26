6b1419ae7b091ae91382006dc35e26a5faade59d
thaiikhang <huynhkhang690@gmail.com>
2023-12-26 10:46:55 +0700

    [Sidebar] Implemented Sidebar UI

---
+++ Frontend/src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Alert,
+  Input,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  AcademicCapIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+ 
+export function Sidebar() {
+  const [open, setOpen] = React.useState(0);
+  const [openAlert, setOpenAlert] = React.useState(true);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+ 
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 flex items-center gap-4 p-4">
+        <img src="https://vnuk.edu.vn/wp-content/uploads/2021/01/vnuk-symbol-only-official-800x237.png" alt="brand" className="w-24 cursor-pointer" />
+        <Typography variant="h5" color="blue-gray">
+          Nemo
+        </Typography>
+      </div>
+      <div className="p-2">
+        <Input icon={<MagnifyingGlassIcon className="h-5 w-5" />} label="Search" />
+      </div>
+      <List>
+        <Accordion
+          open={open === 1}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+            />
+          }
+        >
+          <ListItem className="p-0" selected={open === 1}>
+            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
+              <ListItemPrefix>
+                <PresentationChartBarIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              <Typography color="blue-gray" className="mr-auto font-normal">
+                Class
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1">
+            <List className="p-0">
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                20CSE
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                20DS
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                20IBM
+              </ListItem>
+            </List>
+          </AccordionBody>
+        </Accordion>
+        <hr className="my-2 border-blue-gray-50" />
+        <ListItem>
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Inbox
+          <ListItemSuffix>
+            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
+          </ListItemSuffix>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Profile
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Settings
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+      <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
+        <AcademicCapIcon className="mb-4 h-12 w-12" />
+        <Typography variant="h6" className="mb-1">
+          Give a tip
+        </Typography>
+        <Typography variant="small" className="font-normal opacity-80">
+          The product comes with some features that cannot be utilized at the moment :D
+        </Typography>
+        <div className="mt-4 flex gap-3">
+          <Typography
+            as="a"
+            href="#"
+            variant="small"
+            className="font-medium opacity-80"
+            onClick={() => setOpenAlert(false)}
+          >
+            Dismiss
+          </Typography>
+          <Typography as="a" variant="small" className="font-medium cursor-pointer" onClick={() => setOpenAlert(false)}>
+            Okay, I understand
+          </Typography>
+        </div>
+      </Alert>
+    </Card>
+  );
+}
